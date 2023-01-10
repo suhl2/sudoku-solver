ef2d341fd9480487ce037d83aafc6d491f3be8c7
Sarah <sarah.celiann@gmail.com>
2023-01-10 17:42:44 -0600

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+//get input from the divs
+//build sudoku grid from the divs
+//check to see if grid is valid sudoku puzzle (ie, no doubles, no numbers greater than 9 or less than 1)
+//get row
+//get column
+//get square
+//go through each row, checking to see if 1 can be placed, then 2, etch
+//then go through columns, then squares
+//repeat until sudoku is solved
+//if it runs through all rows, cols, and squares twice with no possible moves, declare puzzle unsolvable
+
+console.log("hey, listen");
